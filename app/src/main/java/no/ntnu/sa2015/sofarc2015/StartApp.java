26793bf1dc8117e98b4403b7e51854fa940bfd43
Markus Lund <markus@teknologia.no>
2015-04-10 12:46:13 +0200

    Changed game activity to main activity

--- app/src/main/java/no/ntnu/sa2015/sofarc2015/StartApp.java
+++ app/src/main/java/no/ntnu/sa2015/sofarc2015/StartApp.java
@@ -14,7 +14,6 @@ public class StartApp extends ActionBarActivity {
         setContentView(R.layout.activity_start_app);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
