27f5286c92e6ffc3da287a2f19bb42b270e2893d
Markus Lund <markus@teknologia.no>
2015-04-12 14:20:16 +0200

    BoardView drawing of board and pieces

--- app/src/main/java/no/ntnu/sa2015/sofarc2015/BoardView.java
+++ app/src/main/java/no/ntnu/sa2015/sofarc2015/BoardView.java
@@ -73,26 +73,39 @@ public class BoardView extends View{
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board.get(0).size(); j++) {
                 String tile = board.get(i).get(j);
-                if (tile.equals("B") || tile.equals("1") || tile.equals("b")) {
-                    paint.setStyle(Paint.Style.FILL);
-                    paint.setColor(Color.BLUE);
-                    paint.setStrokeWidth(3);
-                    canvas.drawRect(j * tileWidth, i * tileWidth, j * tileWidth + tileWidth, i * tileWidth + tileWidth, paint);
-                } else if (tile.equals("R") || tile.equals("2") || tile.equals("r")) {
-                    paint.setStyle(Paint.Style.FILL);
-                    paint.setColor(Color.RED);
-                    paint.setStrokeWidth(3);
-                    canvas.drawRect(j * tileWidth, i * tileWidth, j * tileWidth + tileWidth, i * tileWidth + tileWidth, paint);
-                } else if (tile.equals("G") || tile.equals("4") || tile.equals("g")) {
-                    paint.setStyle(Paint.Style.FILL);
-                    paint.setColor(Color.GREEN);
-                    paint.setStrokeWidth(3);
-                    canvas.drawRect(j * tileWidth, i * tileWidth, j * tileWidth + tileWidth, i * tileWidth + tileWidth, paint);
-                } else if (tile.equals("Y") || tile.equals("3") || tile.equals("y")) {
-                    paint.setStyle(Paint.Style.FILL);
-                    paint.setColor(Color.YELLOW);
-                    paint.setStrokeWidth(3);
-                    canvas.drawRect(j * tileWidth, i * tileWidth, j * tileWidth + tileWidth, i * tileWidth + tileWidth, paint);
+                switch (tile) {
+                    case "B":
+                    case "1":
+                    case "b":
+                        paint.setStyle(Paint.Style.FILL);
+                        paint.setColor(Color.BLUE);
+                        paint.setStrokeWidth(3);
+                        canvas.drawRect(j * tileWidth, i * tileWidth, j * tileWidth + tileWidth, i * tileWidth + tileWidth, paint);
+                        break;
+                    case "R":
+                    case "2":
+                    case "r":
+                        paint.setStyle(Paint.Style.FILL);
+                        paint.setColor(Color.RED);
+                        paint.setStrokeWidth(3);
+                        canvas.drawRect(j * tileWidth, i * tileWidth, j * tileWidth + tileWidth, i * tileWidth + tileWidth, paint);
+                        break;
+                    case "G":
+                    case "4":
+                    case "g":
+                        paint.setStyle(Paint.Style.FILL);
+                        paint.setColor(Color.GREEN);
+                        paint.setStrokeWidth(3);
+                        canvas.drawRect(j * tileWidth, i * tileWidth, j * tileWidth + tileWidth, i * tileWidth + tileWidth, paint);
+                        break;
+                    case "Y":
+                    case "3":
+                    case "y":
+                        paint.setStyle(Paint.Style.FILL);
+                        paint.setColor(Color.YELLOW);
+                        paint.setStrokeWidth(3);
+                        canvas.drawRect(j * tileWidth, i * tileWidth, j * tileWidth + tileWidth, i * tileWidth + tileWidth, paint);
+                        break;
                 }
                 if (tile.equals("#") || tile.equals("1") || tile.equals("2") || tile.equals("3") || tile.equals("4") || tile.equals("g") || tile.equals("y") || tile.equals("r") || tile.equals("b")){
                     paint.setStyle(Paint.Style.STROKE);
